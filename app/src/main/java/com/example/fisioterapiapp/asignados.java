8a84ce5bac278d6a444f0bd5aed48822573308a0
jaison25 <33075690+jaison25@users.noreply.github.com>
2019-10-18 21:34:29 -0500

    esto es un comentario de Jai Master.

--- app/src/main/java/com/example/fisioterapiapp/asignados.java
+++ app/src/main/java/com/example/fisioterapiapp/asignados.java
@@ -13,6 +13,7 @@ public class asignados extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_asignados);
     }
+    //esto es un comentario de Jai Master.
 
     public void buttonRegresar(View view){
         Intent intent = new Intent(asignados.this, menuPrincipal.class);
